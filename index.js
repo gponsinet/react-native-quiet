cb77a5b3645c4e50575efec5f280a0c56c24af66
Godefroy Ponsinet <gponsinet@pm.me>
2020-11-11 18:48:03 +0100

    chore: working transmitter
    Signed-off-by: Godefroy Ponsinet <gponsinet@pm.me>

--- index.js
+++ index.js
@@ -1,47 +1,36 @@
-import { Platform, NativeEventEmitter, NativeModules, PermissionsAndroid } from 'react-native';
+import './web/RNQuiet'
 
-const { RNQuiet } = NativeModules;
+import {
+	NativeEventEmitter,
+	NativeModules,
+	PermissionsAndroid,
+	Platform,
+} from 'react-native'
+const { RNQuiet } = NativeModules
 
-const nativeEventEmitter = new NativeEventEmitter(
-  RNQuiet,
-);
+const nativeEventEmitter = new NativeEventEmitter(RNQuiet)
 
 export default {
-  ...RNQuiet,
-  start: (key) => {
-    if (Platform.OS === 'android') {
-      return Promise
-        .resolve()
-        .then(
-          () => {
-            return PermissionsAndroid
-              .request(
-                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-              )
-              .then(
-                (result) => {
-                  if (result === PermissionsAndroid.RESULTS.GRANTED) {
-                    return RNQuiet
-                      .start(key);
-                  }
-                  return Promise
-                    .reject(
-                      new Error(
-                        'RNQuiet: Unable to start, because we\'re missing the microphone permission.',
-                      ),
-                    );
-                },
-              );
-          },
-        );
-    }
-    return Promise
-      .resolve()
-      .then(() => RNQuiet.start(key));
-  },
-  addListener: listener => nativeEventEmitter
-    .addListener(
-      "onMessageReceived",
-      listener,
-    ),
-};
+	...RNQuiet,
+	start: (key) => {
+		if (Platform.OS === 'android') {
+			return Promise.resolve().then(() => {
+				return PermissionsAndroid.request(
+					PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+				).then((result) => {
+					if (result === PermissionsAndroid.RESULTS.GRANTED) {
+						return RNQuiet.start(key)
+					}
+					return Promise.reject(
+						new Error(
+							"RNQuiet: Unable to start, because we're missing the microphone permission.",
+						),
+					)
+				})
+			})
+		}
+		return Promise.resolve().then(() => RNQuiet.start(key))
+	},
+	addListener: (listener) =>
+		nativeEventEmitter.addListener('onMessageReceived', listener),
+}
